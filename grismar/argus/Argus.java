45b3868e94bd2cb50850a06280ea74a556ebb83f
U-cathedral\grismar <grismar@cathedral.(none)>
2012-03-05 22:36:41 +0100

    initial

---
+++ grismar/argus/Argus.java
@@ -0,0 +1,17 @@
+package grismar.argus;
+
+public class Argus {
+	public void loadFileTypes() {
+		
+	}
+	
+	public void loadLocations() {
+		
+	}
+	
+	public void update() {
+		loadFileTypes();
+		loadLocations();
+		
+	}
+}
---
+++ grismar/minstrel/Minstrel.java
@@ -0,0 +1,12 @@
+package grismar.minstrel;
+
+public class Minstrel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+				
+	}
+
+}
